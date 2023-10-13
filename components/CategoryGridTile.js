34a099bba88015dd6cd9917bb8773bfc706725fb
khalilullahi abdulraheem <kalilulekan@gmail.com>
2023-10-13 23:33:56 +0100

    new

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,49 @@
-import {View, Pressable,Text} from 'react-native'
+import {View, Pressable,Text,StyleSheet, Platform} from 'react-native'
 
 export const CategoryGridTile = ({title, color}) => {
   return (
-    <View>
-        <Pressable>
-            <Text>{title}</Text>
-        </Pressable>
+    <View style={styles.gridItem}>
+    <Pressable
+     style={({pressed}) =>  [ styles.button, pressed ? styles.buttonPressed: null ]}
+    android_ripple={{color: '#ccc'}}>
+     <View style={[styles.innerContainer, {backgroundColor: color}]}>
+     <Text style={styles.title}>{title}</Text>
+    </View>
+    </Pressable>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 8,
+        elevation: 4,
+        textAlign: 'center',
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height:2},
+        shadowRadius: 8,
+        shadowOpacity: 0.25,
+        backgroundColor:'white',
+        overflow:Platform.OS === 'android'? 'hidden' : 'visible'
+    },
+    innerContainer:{
+        flex: 1,
+        padding: 16,
+        borderRadius:8,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+        flex: 1,
+    },
+    buttonPressed:{
+        opacity: 0.35
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
+    }
+})
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -4,12 +4,14 @@ import { CategoryGridTile } from '../components/CategoryGridTile';
 
 function renderCategoryItem (itemData){
     return <CategoryGridTile title={itemData.item.title}
-    color={itemData.item.color} />;
+    color={itemData.item.color} 
+    />;
 }
 export const CategoriesScreen = () => {
   return (
    <FlatList data={CATEGORIES}
-    keyExtractor={(item)=>item.id}
-    renderItem={renderCategoryItem} />
+    keyExtractor={(item) => item.id}
+    renderItem={renderCategoryItem} 
+    numColumns={2}/>
   )
 }
